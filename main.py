7d558df388b02a396b22e048d67cc274b9fab114
TeaganShepherd <teaganshepherd2@gmail.com>
2020-12-18 12:07:27 -0400

    Created main file along with a board and display board function

---
+++ main.py
@@ -0,0 +1,33 @@
+#board
+board = ["-","-","-",
+        "-","-","-",
+        "-","-","-"]
+def displayBoard():
+    print(board[0] + "|" + board[1] + "|" + board[3])
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print(board[8] + "|" + board[7] + "|" + board[8])
+
+
+
+displayBoard()
+
+
+
+
+#display Board
+
+
+#playing Game
+#handle turn
+
+
+
+#isThere a win
+    #check rows
+    #check columns
+    #check diagonals
+
+#cat Game (entire board full + no winner)
+
+
+#switch turns
