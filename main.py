da555688ea5375a3078149f0b7a87823a27e3ab2
TeaganShepherd <teaganshepherd2@gmail.com>
2020-12-24 11:11:54 -0400

    Continuing to work on game logic

--- main.py
+++ main.py
@@ -2,15 +2,58 @@
 board = ["-","-","-",
         "-","-","-",
         "-","-","-"]
+#Player going
+letter = "X"
+
+
 def displayBoard():
     print(board[0] + "|" + board[1] + "|" + board[3])
     print(board[3] + "|" + board[4] + "|" + board[5])
-    print(board[8] + "|" + board[7] + "|" + board[8])
+    print(board[6] + "|" + board[7] + "|" + board[8])
 
 
 
-displayBoard()
+def playGame():
 
+    # Showing the initial Board
+    displayBoard()
+
+    while gameGoing:
+        handleTurn()
+        checkGoing()
+        switchLetter()
+
+
+def checkGoing():
+    checkWin()
+    checkTie()
+
+def checkWin():
+    #check 3 rows
+    #check 3 columns
+    #check 2 diagonals
+    return
+
+def checkTie():
+    #if not win and board full -> there is a tie
+    return
+
+def switchLetter():
+    if(letter is "X"):
+        letter = "O"
+    else:
+        letter = "X"
+
+def handleTurn():
+    position = input("Choose a position from 1-9: ")
+    position = int(position) - 1
+    board[position] = letter
+    displayBoard()
+
+
+
+##Main
+playGame()
 
 
 
@@ -30,4 +73,4 @@ displayBoard()
 #cat Game (entire board full + no winner)
 
 
-#switch turns
+#handle turns
