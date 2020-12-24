a1b9a67b1ce47f102059e35b43f1a623c5bd8b12
TeaganShepherd <teaganshepherd2@gmail.com>
2020-12-24 11:56:19 -0400

    Functional game - there is no error checking. Need to add

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#------------Global Variables--------------
+
 #board
 board = ["-","-","-",
         "-","-","-",
@@ -5,15 +7,22 @@ board = ["-","-","-",
 #Player going
 letter = "X"
 
+#Is game still playable?
+gameGoing = True
+
+#The winner
+winner = None
+#-----------End Global Variables-----------
 
 def displayBoard():
-    print(board[0] + "|" + board[1] + "|" + board[3])
+    print(board[0] + "|" + board[1] + "|" + board[2])
     print(board[3] + "|" + board[4] + "|" + board[5])
     print(board[6] + "|" + board[7] + "|" + board[8])
 
 
 
 def playGame():
+    global gameGoing
 
     # Showing the initial Board
     displayBoard()
@@ -22,23 +31,68 @@ def playGame():
         handleTurn()
         checkGoing()
         switchLetter()
+    
+    #Game ends once we break the loop
+    if (winner == None):
+        print ("There was no winner. Tie game")
+    else:
+        print ("The winner was: " + winner)
 
 
 def checkGoing():
+    global gameGoing
     checkWin()
-    checkTie()
+    if(gameGoing):
+        checkTie()
 
 def checkWin():
+    global gameGoing
+    global winner
     #check 3 rows
+    #X win
+    if( (board[0] == board[1] == board[2] == "X") or (board[3] == board[4] == board[5]  == "X") or (board[6] == board[7] == board[8]  == "X")  ):
+        winner = "X"
+        gameGoing = False
+    #O win
+    if( (board[0] == board[1] == board[2] == "O") or (board[3] == board[4] == board[5]  == "O") or (board[6] == board[7] == board[8]  == "O")  ):
+        winner = "O"
+        gameGoing = False
+    
     #check 3 columns
+     #X win
+    if( (board[0] == board[3] == board[6] == "X") or (board[1] == board[4] == board[7]  == "X") or (board[2] == board[5] == board[8]  == "X")  ):
+        winner = "X"
+        gameGoing = False
+    #O win
+    if( (board[0] == board[3] == board[6] == "O") or (board[1] == board[4] == board[7]  == "O") or (board[2] == board[5] == board[8]  == "O")  ):
+        winner = "O"
+        gameGoing = False
+
     #check 2 diagonals
-    return
+     #X win
+    if( (board[0] == board[4] == board[8] == "X") or (board[2] == board[4] == board[6]  == "X")  ):
+        winner = "X"
+        gameGoing = False
+    #O win
+    if( (board[0] == board[4] == board[8] == "O") or (board[2] == board[4] == board[6]  == "O") ):
+        winner = "O"
+        gameGoing = False
+    if(gameGoing is False):
+        return True
+    return False
 
 def checkTie():
+    global gameGoing
     #if not win and board full -> there is a tie
-    return
+    full = True
+    for mark in board:
+        isMarked = not(mark is "X" or mark is "O")
+        if(isMarked):
+            full = False
+    gameGoing = (checkWin() or not full)
 
 def switchLetter():
+    global letter
     if(letter is "X"):
         letter = "O"
     else:
